af1b543754b64756cc7ee8d3bfe53c51557ef8d9
D.Tadpole <gsk390@gmail.com>
2023-12-17 09:49:42 +0000

    tune

--- train.py
+++ train.py
@@ -41,8 +41,8 @@ parser.add_argument('--lr', default=3e-4, type=float,
                     help="learning rate (default: 3e-4)")
 parser.add_argument('--lr_end', default=1e-5, type=float,
                     help="ending learning rate (default: 1e-5)")
-parser.add_argument('--accumulate_grad', default=8, type=int,
-                    help="accumulate gradient (default: 8)")
+parser.add_argument('--accumulate_grad', default=4, type=int,
+                    help="accumulate gradient (default: 4)")
 parser.add_argument('--gradient_clipping', default=1.0, type=float,
                     help="gradient clipping (default: 1.0)")
 
